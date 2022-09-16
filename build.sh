57769c77f9391c76fe88d6bc617b6251bb62848f
Ziyao <tswuyin_st127@163.com>
2022-09-16 15:44:18 +0000

    Import working codebase from original binutils.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+clang bucomm.c objcopy.c bfd/libbfd.a -I. -I./include -I./bfd -o objdump libiberty/libiberty.a -lz version.c rename.c filemode.c rddbg.c debug.c coffgrok.c stabs.c wrstabs.c rdcoff.c
