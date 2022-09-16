36428ea646163b7c7b17fb2c4b47f33b737fe933
Ziyao <tswuyin_st127@163.com>
2022-09-16 16:23:26 +0000

    Fix wrong filename again.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 clang bucomm.c objcopy.c bfd/libbfd.a -I. -I./include -I./bfd	\
-       	-o binutils-objdump libiberty/libiberty.a		\
+       	-o binutils-objcopy libiberty/libiberty.a		\
 	-lz version.c rename.c filemode.c rddbg.c debug.c	\
 	coffgrok.c stabs.c wrstabs.c rdcoff.c
